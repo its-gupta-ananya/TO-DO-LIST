1586047403c90631546c71e0d18cc0e3b25b6f3e
Ananya Gupta <its.gupta.ananya@gmail.com>
2020-07-29 19:51:34 +0530

    To-do-list - CSS improvement req

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const Task = require("./config/mongoose");
+const expressLayouts = require('express-ejs-layouts');
 const { futimesSync } = require('fs');
 
-app.set(express.static('assets'));
+app.use(express.static('assets'));
+app.use(expressLayouts);
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 app.use(express.urlencoded());
 
 app.use('/',require('./routes'));
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+
 
 app.listen(port,function(err)
 {
--- models/To-do.js
+++ models/To-do.js
@@ -11,7 +11,7 @@ const ItemSchema = mongoose.Schema
     },
     Due_date:
     {
-        type: Date,
+        type: String,
         required: true,
         
     }
